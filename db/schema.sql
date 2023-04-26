467620f0c26a3d2fa574e5dba84164543c72a5b0
Grace Roh <grace@BIGMINE.attlocal.net>
2023-04-25 23:35:52 -0700

    add code to seed database

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,2 @@
-DROP DATABASE IF EXISTS cheesecakecode_db;
-CREATE DATABASE cheesecakecode_db;
+DROP DATABASE IF EXISTS cheesecake_db;
+CREATE DATABASE cheesecake_db;
---
+++ seeds/index.js
@@ -0,0 +1,28 @@
+const sequelize = require('../config/connection');
+const { User, BlogPost, Comment } = require('../model');
+
+const userData = require('./userData.json');
+const blogPostData = require('./blogPostData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    const blogPosts = await BlogPost.bulkCreate(blogPostData, {
+        returning: true,
+    });
+
+    const comments = await Comment.bulkCreate(commentData, {
+        returning: true,
+    })
+
+    process.exit(0);
+}
+
+seedDatabase();
+
