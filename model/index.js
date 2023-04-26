94f575b5a7fe7f8557ddf653851d641fcfd83fcb
Grace Roh <grace@BIGMINE.attlocal.net>
2023-04-25 21:18:56 -0700

    add relationships between models

---
+++ model/index.js
@@ -0,0 +1,36 @@
+const User = require('./User');
+const BlogPost = require('./BlogPost');
+const Comment = require('./Comment');
+
+//User has MANY BlogPosts
+User.hasMany(BlogPost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+//but BlogPosts only has ONE User
+BlogPost.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+//User has MANY Comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+//but Comments only has ONE User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+//BlogPost has MANY Comments
+BlogPost.hasMany(Comment, {
+    foreignKey: 'blogpost_id',
+    onDelete: 'CASCADE',
+});
+
+//but Comments only has ONE BlogPost
+Comment.belongsTo(BlogPOst, {
+    foreignKey: 'blogpost_id',
+});
