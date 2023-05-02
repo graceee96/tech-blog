e18b4592dc71a3eacc24b5aaad629797e2bba7e8
Grace Roh <grace@BIGMINE.attlocal.net>
2023-05-02 13:39:35 -0700

    edit connection.js

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -1,3 +1,9 @@
+//character counter
+let blogContent = document.getElementById('editpost-content');
+let characters = document.getElementById('character');
+let currentCharactersLeft = 255 - blogContent.value.length;
+characters.textContent = currentCharactersLeft;
+
 //fetch request to update new post to /api/blogposts
 const editPost = async (event) => {
     event.preventDefault();
@@ -5,27 +11,25 @@ const editPost = async (event) => {
     const title = document.querySelector('#editpost-title').value.trim();
     const content = document.querySelector('#editpost-content').value.trim();
 
-    if (title && content) {
-        const response = await fetch('/api/blogposts/:id', {
-            method: 'PUT',
-            body: JSON.stringify({ title, content }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+    if (event.target.getAttribute('data-postid')) {
+        const id = event.target.getAttribute('data-postid');
+        
+        if (title && content) {
+            const response = await fetch('/api/blogposts/:id', {
+                method: 'PUT',
+                body: JSON.stringify({ title, content }),
+                headers: { 'Content-Type': 'application/json' },
+            });
 
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert('Failed to update blog post');
-            console.log(response)
-        };
+            if (response.ok) {
+                document.location.replace('/dashboard');
+            } else {
+                alert('Failed to update blog post');
+                console.log(response)
+            };
+        }
     }
 }
-
-//character counter
-let blogContent = document.getElementById('editpost-content');
-let characters = document.getElementById('character');
-let currentCharactersLeft = 255 - blogContent.value.length;
-characters.textContent = currentCharactersLeft;
 
 blogContent.addEventListener('input', () => {
     let charactersLeft = 255 - blogContent.value.length;
