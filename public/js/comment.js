cdc4bec8256cdc510d4253a987372ed905c873e6
Grace Roh <grace@BIGMINE.attlocal.net>
2023-05-03 18:56:54 -0700

    get rid of character counter in blogPost comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,19 +1,6 @@
-//character counter
-let commentContent = document.getElementsByClassName('comment-text');
-let characters = document.getElementsByClassName('characters');
-let currentCharactersLeft = 255 - commentContent.value.length;
-characters.textContent = currentCharactersLeft;
-
 //fetch request to update comment to /api/comments/:id
 
 //fetch request to delete comment to /api/comments/:id
-
-//event listener for character counter
-commentContent.addEventListener('input', () => {
-    let charactersLeft = 255 - commentContent.value.length;
-
-    characters.textContent = charactersLeft;
-})
 
 //event listener for updating comments
 
