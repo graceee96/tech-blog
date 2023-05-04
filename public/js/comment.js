93700bf46998d87a384fe36adc4fc700e84862a9
Grace Roh <grace@BIGMINE.attlocal.net>
2023-05-03 20:50:58 -0700

    tried to add functionality to add comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,6 +1,23 @@
 //fetch request for adding comments to /api/comments
 const addComment = async (event) => {
     event.preventDefault();
+    console.log(event.target);
+
+    const comment = document.querySelector('#new-comment').value.trim();
+
+    if (comment) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ comment }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            console.log('new comment!')
+        } else {
+            alert('Failed to add new comment!')
+        }
+    }
 }
 
 //fetch request to update comment to /api/comments/:id
@@ -11,11 +28,27 @@ const editComment = async (event) => {
 //fetch request to delete comment to /api/comments/:id
 const deleteComment = async (event) => {
     event.preventDefault();
-    console.log(event)
+    console.log(event.target)
+
+    // if (event.target.hasAttribute('data-id')) {
+    //     const id = event.target.getAttribute('data-id');
+
+    //     const res = await fetch(`/api/comments/${id}`, {
+    //         method: 'DELETE',
+    //     });
+
+    //     if (res.ok) {
+    //         document.location.replace('/blogpost')
+    //     } else {
+    //         alert('Failed to delete comment')
+    //     }
+    // }
 }
 
 //event listener for adding comments
+document.querySelector('#addcomment-btn').addEventListener('submit', addComment)
 
 //event listener for updating comments
 
 //event listener for deleting comments
+// document.querySelector('#comment-list').addEventListener('click', deleteComment);
