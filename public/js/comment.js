386385f975ceae1a468bf0d45c4a9552e8f4b529
Grace Roh <grace@BIGMINE.local>
2023-05-03 19:06:59 -0700

    add date commented

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,6 +1,19 @@
+//fetch request for adding comments to /api/comments
+const addComment = async (event) => {
+    event.preventDefault();
+}
+
 //fetch request to update comment to /api/comments/:id
+const editComment = async (event) => {
+    event.preventDefault();
+}
 
 //fetch request to delete comment to /api/comments/:id
+const deleteComment = async (event) => {
+    event.preventDefault();
+}
+
+//event listener for adding comments
 
 //event listener for updating comments
 
